fadefc4a98fab88ff52c4fc0da2663a0895f5a5e
Eli Livshitz <eli.livshitz@gmail.com>
2019-04-04 23:38:57 +0300

    + angularEx * adjusting bundular to actually represent angularEx

--- src/scripts/browserify/libx.essentials.browser.js
+++ src/scripts/browserify/libx.essentials.browser.js
@@ -1,3 +1,3 @@
 global.libx = require('libx.js/bundles/browser.essentials');
 if (global._ == null) global._ = libx._;
-global.libx.browser.angular = require('libx.js/browser/angular');
+global.libx.browser.angular = require('../../../modules/angular-ex');
